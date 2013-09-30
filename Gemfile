65859d3c543ef84b0dee1cf3978e599ce846fd84
dbc-warrior <apprentice@devbootcamp.com>
2013-09-30 15:51:02 -0700

    fb login skeleton

--- Gemfile
+++ Gemfile
@@ -1,12 +1,18 @@
 
 source "https://rubygems.org"
 
-
 gem "sinatra"
+gem "activerecord"
 gem "sinatra-activerecord"
 gem "shotgun"
 gem "tux"
+gem "omniauth"
+gem "omniauth-facebook"
 
 group :production do
   gem "pg"
+end
+
+group :development do
+  gem "sqlite3"
 end
--- app.rb
+++ app.rb
@@ -1,6 +1,31 @@
+
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'omniauth-facebook'
+require 'sqlite3'
+require_relative 'models/user'
 
+
+SCOPE = 'email,read_stream'
+enable :sessions
+
+set :database, "sqlite3:///tables.db"
 get '/' do
-  erb :home
+  redirect '/auth/facebook'
+
 end
+
+get '/auth/:provider/callback' do
+  # we can do something special here is +state+ param is canvas
+  # (see notes above in /canvas/ method for more details)
+  content_type 'application/json'
+  MultiJson.encode(request.env['omniauth.auth'])
+end
+
+use OmniAuth::Builder do
+  provider :facebook, '240808996072944', 'c71de27a51fd8151c93d172d72bce0a9', :scope => SCOPE
+end
+
+
+
+
