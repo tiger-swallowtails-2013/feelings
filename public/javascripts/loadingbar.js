0b99cc8a1fd5a4fd67a87ae54d16cf2bbc8f6428
Apprentice <apprentice@dbc12.local>
2013-10-03 15:54:27 -0700

    added loading image

---
+++ public/javascripts/loadingbar.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#gimmesongs").submit(function() {
+   $(".loading_image").fadeIn(2000);
+   setTimeout( function(){
+    loading_image.style.visibility= 'hidden';
+  };
+  , 18000);
+ });
+});
+
+
+
+
+
+
+
+
