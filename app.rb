2cc7fa5fd70dff84081d3eb3737aebe8349a473a
Apprentice <apprentice@dbc12.local>
2013-10-02 23:15:19 -0700

    breaks out of loop if too many queries

--- app.rb
+++ app.rb
@@ -56,6 +56,7 @@ helpers do
   end
 
   def get_playlist(params)
+    request_count = 0
     current_mood = URI::escape(params[:current_mood])
     desired_mood = URI::escape(params[:desired_mood])
     style = URI::escape(params[:style])
@@ -63,7 +64,8 @@ helpers do
     @playlist = []
 
     begin
-
+      request_count += 1
+      break if request_count > 20
       songs = get_songs(current_mood,desired_mood, style, x, y)
 
       songs.each do |song|
@@ -72,14 +74,12 @@ helpers do
           break
         end
       end
-
     end while @playlist.length <= 10
 
     spotify_playlist = []
     @playlist.each do |song|
       spotify_playlist << query_spotify(song['artist_name'],song['title'])
     end
-    p spotify_playlist
     spotify_playlist
   end
 
@@ -104,7 +104,6 @@ helpers do
     "&artist_min_hotttnesss=0.25"+
     "&sort=song_hotttnesss-desc"
     uri = URI(URI.encode(uri_string))
-    puts uri
     response = Net::HTTP.get(uri)
     hash = JSON.parse(response)
     result = hash["response"]["songs"]
@@ -121,7 +120,6 @@ helpers do
   end
 
   def query_spotify(artist_name, song_title)
-    p "query spotify"
     artist = URI::escape(artist_name).gsub(/&/, "and")
     uri = "http://ws.spotify.com/search/1/track.json?q="
     request = URI("#{uri}#{artist}")
@@ -130,7 +128,6 @@ helpers do
   end
 
   def get_spotify_song_id(all_tracks, song_title)
-    p "get spotify song id"
     result = all_tracks["tracks"].select { |track| track["name"].include? song_title}
     result.empty? ? nil : result[0]["href"].gsub(/spotify:track:/, "")
   end
