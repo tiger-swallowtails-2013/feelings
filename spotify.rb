844d09f4a23b35427069b6fa9b8dd2c21f5dc1b1
Jonathan Chew <jon.j.chew@gmail.com>
2013-10-06 02:07:16 -0700

    refactored. Still need to decouple hydras

--- spotify.rb
+++ spotify.rb
@@ -6,29 +6,23 @@ module Spotify
 
 
 	def self.query_id(all_tracks, song_title)
-		puts "HEYTHERE"
-		p all_tracks
-		puts "TRACKSSS"
-		p all_tracks["tracks"]
 		result = all_tracks["tracks"].select { |track| track["name"].include? song_title}
 		results_in_us = playable_in_us?(result)
 		results_in_us.empty? ? nil : results_in_us[0]["href"].gsub(/spotify:track:/, "")
 	end
 
 
-	def self.get_songs(playlist)
-		track_title_array = []
-		spotify_playlist = Array.new(playlist.length)
-		requests = []
-		hydra = Typhoeus::Hydra.new
 		# 1. Looks up song in database
 		# 2. Populates array with spotify ids, and nils if none
 		# 3. If song isn't in database, put in hydra queue
 		# 4. Run hydra
 		# 5. Populate artist_array with responses
 		# 6. Fill in the spotify_playlist nils with the spotify ids from artist_array one by one
+	def self.get_songs(playlist)
+		spotify_playlist = Array.new(playlist.length)
+		requests = []
+		hydra = Typhoeus::Hydra.new
 		playlist.each_with_index do |song, index|
-			track_title_array << song["title"]
 			song_in_db = Song.find_or_create_by(artist_name: song["artist_name"], title: song["title"])
 			unless song_in_db.spotify_id.nil?
 				spotify_playlist[index] = song_in_db.spotify_id
@@ -46,20 +40,11 @@ module Spotify
 		artist_array = requests.map { |request| JSON.parse(request.response.response_body) }
 			spotify_playlist.each_with_index do |song,index|
 			if song.nil?
-				p spotify_playlist
-				puts "ARTISTARRAY at #{index}"
-				p artist_array
+				title = playlist[index]["title"]
 				artist_tracks = artist_array.shift
-				puts "ARTISTTRACKS #{index}"
-				p artist_tracks
-				title = track_title_array[index]
 				spotify_id = query_id(artist_tracks,title)
 				spotify_playlist[index] = spotify_id
-				song_in_db = Song.where(artist_name: playlist[index]["artist_name"], title: playlist[index]["title"])
-				puts "MONKEY"
-				p artist_tracks["info"]["query"]
-				p title
-				p song_in_db
+				song_in_db = Song.where(artist_name: playlist[index]["artist_name"], title: title)
 				song_in_db[0].update_attribute(:spotify_id, spotify_id)
 			end
 		end
