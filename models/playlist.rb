de973631855dcefe588c676b1d6290875e98edee
Jonathan Chew <jon.j.chew@gmail.com>
2013-10-06 17:00:50 -0700

    nvm, fixed, deleted empty init method in playlist model

--- models/playlist.rb
+++ models/playlist.rb
@@ -1,9 +1,7 @@
 class Playlist < ActiveRecord::Base 
 	belongs_to :user
 	# validates :name, presence: true, length: {minimum: 2, maximum: 30, message: "Must be between 2 and 30 characters"}
-  def initialize
-    
-  end
+  
 end
 
 
